20e6a1476a61ac80c7d7205adc86d8a412f2d754
breadyboi <95249362+NockKnock@users.noreply.github.com>
2023-11-29 17:04:21 -0500

    Create IntakeSubsystem.java

---
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -0,0 +1,38 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+
+public class IntakeSubsystem extends SubsystemBase {
+  CANSparkMax leftIntakeMotor;
+  CANSparkMax rightIntakeMotor;
+  
+  /** Creates a new IntakeSubsystem. */
+  public IntakeSubsystem(CANSparkMax leftIntakeMotor, CANSparkMax rightIntakeMotor) {
+ 
+    leftIntakeMotor = new CANSparkMax(id, MotorType.kBrushless);
+    rightIntakeMotor = new CANSparkMax(id, MotorType.kBrushless);
+  }
+
+  public void runMotors(double speed) {
+    leftIntakeMotor.set(speed);
+    rightIntakeMotor.set(speed);
+  }
+
+  public void stopMotors() {
+    leftIntakeMotor.stopMotor();
+    rightIntakeMotor.stopMotor();
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
